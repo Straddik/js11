500c0982c96526a6d9679c512f761f3f22838064
Anastasiya Shkrogal <straddi@mail.ru>
2020-01-24 11:25:12 +0300

    Выполнено основное задание урока №2

---
+++ Lesson2/Main/js/script.js
@@ -0,0 +1,26 @@
+// 1) объявление переменных и присвоение значений по заданию
+let money = 60000,
+    income = "Profi.ru",
+    addExpenses = "Коммунальные платежы, продукты, кружки, проезд",
+    deposit = true,
+    mission = 140000,
+    period = 5;
+
+// 2) вывод в консоль
+console.log("Тип переменной money: " + typeof(money));
+console.log("Тип переменной income: " + typeof(income));
+console.log("Тип переменной deposit: " + typeof(deposit));
+console.log("Длина строки addExpenses: " + addExpenses.length);
+console.log("Период равен " + period + " месяцев");
+console.log(`Цель заработать ${mission} рублей`);
+addExpenses = addExpenses.toLowerCase().split(',')
+console.log("После приведения строки addExpenses к нижнему регистру и преобразованию в массив : " + addExpenses);
+
+let budgetDay = money / 30;
+
+console.log("Дневной бюджет равен " + budgetDay);
+
+
+
+
+// 4) Добавить папку или ветку со вторым уроком в свой репозиторий на GitHub
