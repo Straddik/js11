4c32a4bab6139aa4d4410eeb293302fa39cbfbe2
Anastasiya Shkrogal <straddi@mail.ru>
2020-02-13 20:43:29 +0300

    Добавлена латиница

--- Lesson15/js/index.js
+++ Lesson15/js/index.js
@@ -64,7 +64,7 @@ class AppData {
                 e.target.value = e.target.value.replace(/[^\d]/g, '');
             }));
             items.forEach(item => item.querySelector(`.${stringStart}-title`).addEventListener('input', (e) => {
-                e.target.value = e.target.value.replace(/[^а-я А-Я \W]/g, '');
+                e.target.value = e.target.value.replace(/[^а-я А-Я a-z A-Z \W]/g, '');
             }));
             if (items.length === 3 && stringStart === 'expenses') {
                 buttonPlusExpense.style.display = 'none';
@@ -273,22 +273,22 @@ class AppData {
             inputTargetAmount.value = inputTargetAmount.value.replace(/[^\d]/g, '');
         });
         inputAdditionalExpenseAmount.addEventListener('input', () => {
-            inputAdditionalExpenseAmount.value = inputAdditionalExpenseAmount.value.replace(/[^а-я А-Я \W]/g, '');
+            inputAdditionalExpenseAmount.value = inputAdditionalExpenseAmount.value.replace(/[^а-я А-Я a-z A-Z \W]/g, '');
         });
         inputAdditionalIncomeItems.forEach((item) => item.addEventListener('input', () => {
-            item.value = item.value.replace(/[^а-я А-Я \W]/g, '');
+            item.value = item.value.replace(/[^а-я А-Я a-z A-Z \W]/g, '');
         }));
         inputIncomeItems.forEach(item => item.querySelector('.income-amount').addEventListener('input', (e) => {
             e.target.value = e.target.value.replace(/[^\d]/g, '');
         }));
         inputIncomeItems.forEach(item => item.querySelector('.income-title').addEventListener('input', (e) => {
-            e.target.value = e.target.value.replace(/[^а-я А-Я \W]/g, '');
+            e.target.value = e.target.value.replace(/[^а-я А-Я a-z A-Z \W]/g, '');
         }));
         expensesItems.forEach(item => item.querySelector('.expenses-amount').addEventListener('input', (e) => {
             e.target.value = e.target.value.replace(/[^\d]/g, '');
         }));
         expensesItems.forEach(item => item.querySelector('.expenses-title').addEventListener('input', (e) => {
-            e.target.value = e.target.value.replace(/[^а-я А-Я \W]/g, '');
+            e.target.value = e.target.value.replace(/[^а-я А-Я a-z A-Z \W]/g, '');
         }));
     };
 
